007bd5db54b5a7180cab7b8fe717d6d9371732a9
phil27ajs <judahsamalagi@gmail.com>
2025-02-27 14:03:14 +0700

    banner autoslide & cb validation form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+
+function validateForm() {
+    let nameInput = document.getElementById("name").innerHTML;
+    
+    if (nameInput == "") {
+        alert("Name cannot be empty");
+    }
+}
+
+// Banner Autoslide
+
 let indexBanner = 0;
 
-function increase() {
-    indexBanner += 1;
+function nextBanner() {
+    indexBanner += 2;
     showBanner();
 }
 
@@ -11,11 +22,12 @@ function showBanner() {
     if (indexBanner > (banner.length - 1)) {
         indexBanner = 0;
     }
-    
-    for (let i = 0; i < banner.length; i++) {
-        banner[i].style.display = "none";
+
+    for (let i = 0; i < banner.length; i += 2) {
+        banner[i].style.display = 'none';
+        banner[i + 1].style.display = 'none';
     }
     banner[indexBanner].style.display = 'block';
+    banner[indexBanner + 1].style.display = 'block';
 }
-
-setInterval(increase, 3000)
+setInterval(nextBanner, 3000)
